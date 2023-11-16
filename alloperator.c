9d9a0f1108fe1a03fad3684f59a39aae7b93a0d1
UpnishSingh <146972517+UpnishSingh@users.noreply.github.com>
2023-11-16 23:35:31 +0530

    Add files via upload

---
+++ alloperator.c
@@ -0,0 +1,21 @@
+// All mathematical operations in a single code
+
+#include<stdio.h>
+int main()
+{
+    int a,b,addition,subtraction,multiplication,division;
+    printf("enter the two numbers");
+    scanf("%d%d",&a,&b);
+    addition=a+b;
+    printf("sum=%d\n",a+b);
+    subtraction=a-b;
+    printf("subtraction=%d\n",a-b);
+    multiplication=a*b;
+    printf("multiplication=%d\n",a*b); 
+    division=a%b;
+    printf("division=%d\n",a%b);
+    return 0;
+
+    
+
+}
